5e6f43eb3fa076fcc34702c8ae14509e2635feff
Liman2009 <liman@github.com>
2024-09-27 08:31:08 +0600

    Auto workflow build system

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liman2009/gocode
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import ("
+  fmt
+")
+
+var version = "0.0.0"
+
+func main() {
+	fmt.println("Hello World ", version)
+}
