338b64d75b82a975eca6cde07e4f02af7278b6a1
Liman2009 <liman@github.com>
2024-09-28 08:21:21 +0600

    Gin added with /info route

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
    "fmt"
+   "net/http"
+   "github.com/gin-gonic/gin"
 )
 
-var version = "0.0.0"
+var version string = "0.0.0"
 
 func main() {
-	fmt.Println("Hello World ", version)
+	r := gin.Default()
+        r.GET("/info", func(c *gin.Context) {
+          c.JSON(http.StatusOK, gin.H{
+            "name": "gocode",
+	    "author": "liman",
+	    "version": version,
+          })
+        })
+	fmt.Println("Server running on version", version)
+	r.Run()
 }
